ba4711ff2041d5ba2d437283197e5ae2a8d82a40
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-12 23:19:44 +0100

    milestone 2 and 3 completed, enhanced code readibility

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,43 @@
 const button = document.getElementById('generate');
+// Constant variables of the form, input and table ID
+const trainForm = document.getElementById('fill');
+const userName = document.getElementById('name');
 const userKilometers = document.getElementById('kilometers');
 const userAge = document.getElementById('age');
+const tableBody = document.getElementById('table-body');
 
-button.addEventListener('click', function () {
+// Add an event listener to prevent it from losing user data with automatic page reload and update the table with user data and random generated numbers
+// Generating random numbers for "Offerta", "Carrozza" and "Codice CP" with Math.random
+// Calculating price based on userAge and userKilometers
+// Generating new td inside the tbody with .innerHTML
 
-    let price = parseFloat(userKilometers.value) * 0.21;
+trainForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(userName.value, userKilometers.value, userAge.value);
     let offer;
-
+    let carriage = Math.floor(Math.random() * 6 + 1);
+    let code = Math.floor(Math.random() * 9999 + 1);
+    let price = parseFloat(userKilometers.value) * 0.21;
     if (userAge.value === 'minor') {
         price = price - (price * 20) / 100;
         offer = 'Offerta Minorenni';
+        console.log("20% di sconto!");
     } else if (userAge.value === 'old') {
         price = price - (price * 40) / 100;
         offer = 'Offerta Anziani';
+        console.log("40% di sconto!");
     } else {
         offer = 'Standard';
+        console.log("Nessuno sconto applicato");
     }
-
-    console.log("Offerta:", offer);
-    console.log("Prezzo finale:", price);
-});
+    console.log(offer, carriage, code, price);
+    tableBody.innerHTML += `
+                        <tr>
+                            <td>${userName.value}</td>
+                            <td>${offer}</td>
+                            <td>${carriage}</td>
+                            <td>${code}</td>
+                            <td>${price.toFixed(2)}</td>
+                        </tr>
+                            `
+})
