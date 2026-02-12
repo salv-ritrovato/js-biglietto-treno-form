b948cfb894ee93c6b94f3a94d220d23df6d44228
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-12 22:46:24 +0100

    milestone one completed successfully

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-console.log('it works');
+const button = document.getElementById('generate');
+const userKilometers = document.getElementById('kilometers');
+const userAge = document.getElementById('age');
+
+button.addEventListener('click', function () {
+
+    let price = parseFloat(userKilometers.value) * 0.21;
+    let offer;
+
+    if (userAge.value === 'minor') {
+        price = price - (price * 20) / 100;
+        offer = 'Offerta Minorenni';
+    } else if (userAge.value === 'old') {
+        price = price - (price * 40) / 100;
+        offer = 'Offerta Anziani';
+    } else {
+        offer = 'Standard';
+    }
+
+    console.log("Offerta:", offer);
+    console.log("Prezzo finale:", price);
+});
